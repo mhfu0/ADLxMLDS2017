6854f9df453cf318ac2bb6ffa31442c9b753b567
mhfu0 <r06922030@ntu.edu.tw>
2017-12-15 23:03:32 +0800

    pg python script fix

--- hw3/agent_dir/agent_pg.py
+++ hw3/agent_dir/agent_pg.py
@@ -14,11 +14,8 @@ from agent_dir.agent import Agent
 LEARNING_RATE = 1e-4
 NUM_EPISODES = 100000
 MAX_NUM_STEPS = 10000
-BATCH_SIZE = 32
 
-OBSERVATIONS_SIZE = 6400
-
-# Action values to send to gym environment to move paddle up/down
+# Action values to send to gym environment
 UP_ACTION = 2
 DOWN_ACTION = 3
 
@@ -35,21 +32,20 @@ class Agent_PG(Agent):
         config.gpu_options.allow_growth=True
         
         # Parameter settings
-        self.checkpoints_dir = 'pg_network_conv_rmsprop_%.2f/' % (args.gamma)
+        self.checkpoints_dir = 'pg_network_conv_rmsprop_0.99/'
         self.gamma = args.gamma
 
         self.time_step = 0
         self.learning_rate = LEARNING_RATE
         self.state_dim = [80,80,1]
         #self.actions = env.action_space.n
+        self.reward_history = []
         
         # Reproducibility
         random.seed(1239)
         np.random.seed(1239)
         tf.set_random_seed(1239)
         #self.env.seed(1239)
-        
-        self.reward_history = []
         
         # Build network
         self.create_network()
@@ -58,14 +54,12 @@ class Agent_PG(Agent):
         self.sess.run(tf.global_variables_initializer())
         
         self.saver = tf.train.Saver()
-        self.checkpoint_file = os.path.join(self.checkpoints_dir,'pg_network.ckpt')
+        self.checkpoint_file = os.path.join(self.checkpoints_dir, 'pg_network.ckpt')
         
         if args.test_pg:
-            #you can load your model here
-            print('loading trained model')
-            self.checkpoint_file = os.path.join(self.checkpoints_dir,'pg_network-Copy3.ckpt')
+            print('Loading model parameters...')
+            self.checkpoint_file = os.path.join(self.checkpoints_dir,'pg_network-Copy5.ckpt')
             self.load_checkpoint()
-
 
     def init_game_setting(self):
         self.test_last_observation = self.env.reset()
@@ -77,11 +71,11 @@ class Agent_PG(Agent):
         self.actions = []
         self.rewards = []
         
-        smoothed_reward = None
+        reward_moving_avg = None
         episode_n = 1
         
         while True:
-            print("Starting episode %d" % episode_n)
+            print('Starting episode %d...' % episode_n)
             episode_done = False
             episode_reward_sum = 0
             round_n = 1
@@ -110,45 +104,36 @@ class Agent_PG(Agent):
                 self.states.append(observation_delta)
                 self.actions.append(action_dict[action])
                 self.rewards.append(reward)
-                #tup = (observation_delta, action_dict[action], reward)
-                #batch_state_action_reward_tuples.append(tup)
 
                 if reward == -1:
-                    print("Round %d: %d time steps; lost..." % (round_n, n_steps))
+                    print('Round %d: %d timesteps; lost...' % (round_n, n_steps))
                 elif reward == +1:
-                    print("Round %d: %d time steps; won!" % (round_n, n_steps))
+                    print('Round %d: %d timesteps; won!' % (round_n, n_steps))
                 if reward != 0:
                     round_n += 1
                     n_steps = 0
 
-            print("Episode %d finished after %d rounds" % (episode_n, round_n))
+            print('Episode %d finished after %d rounds' % (episode_n, round_n-1))
 
             # exponentially smoothed version of reward
-            if smoothed_reward is None:
-                smoothed_reward = episode_reward_sum
+            if reward_moving_avg is None:
+                reward_moving_avg = episode_reward_sum
             else:
-                smoothed_reward = smoothed_reward * 0.99 + episode_reward_sum * 0.01
-            print("Reward total was %.3f; discounted moving average of reward is %.3f" \
-                % (episode_reward_sum, smoothed_reward))
+                reward_moving_avg = reward_moving_avg * 0.99 + episode_reward_sum * 0.01
+            print('Reward total was %.3f; discounted moving average of reward is %.3f' \
+                % (episode_reward_sum, reward_moving_avg))
             self.reward_history.append(episode_reward_sum)
-
-            #if episode_n % args.batch_size_episodes == 0:
-            #states, actions, rewards = zip(*batch_state_action_reward_tuples)
             
             self.rewards = self.discount_rewards(self.rewards, self.gamma)
             self.rewards -= np.mean(self.rewards)
             self.rewards /= np.std(self.rewards)
-            #batch_state_action_reward_tuples = list(zip(states, actions, rewards))
-            #self.train_network(batch_state_action_reward_tuples)
             self.train_network(self.states, self.actions, self.rewards)
-            #batch_state_action_reward_tuples = []
             self.states, self.actions, self.rewards = [], [], []
-            
-
             ### Episode ends here ###
+            
             if episode_n % 250 == 0:
                 self.save_checkpoint()
-                with open(os.path.join(self.checkpoints_dir,'pg_reward_history.pickle'), 'wb') as f:
+                with open(os.path.join(self.checkpoints_dir, 'pg_reward_history.pickle'), 'wb') as f:
                     pickle.dump(self.reward_history, f)
                 print('Model saved and reward history dumped...')
 
@@ -164,19 +149,6 @@ class Agent_PG(Agent):
         return action
     
     def create_network(self):
-        '''
-        self.observations = tf.placeholder(tf.float32, [None] + self.state_dim)
-        self.sampled_actions = tf.placeholder(tf.float32, [None, 1])
-        self.advantage = tf.placeholder(tf.float32, [None, 1])
-        
-        h = tf.layers.dense(self.observations,units=200,activation=tf.nn.relu,
-            kernel_initializer=tf.contrib.layers.xavier_initializer())
-        self.up_probability = tf.layers.dense(h,units=1,activation=tf.sigmoid,
-            kernel_initializer=tf.contrib.layers.xavier_initializer())
-        self.loss = tf.losses.log_loss(labels=self.sampled_actions,predictions=self.up_probability,weights=self.advantage)
-        optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        self.train_op = optimizer.minimize(self.loss)
-        '''
         # input layer
         self.observations = tf.placeholder(tf.float32,[None] + self.state_dim)
         self.sampled_actions = tf.placeholder(tf.float32, [None, 1])
@@ -198,28 +170,19 @@ class Agent_PG(Agent):
         h_conv2_flat = tf.reshape(h_conv2,[-1,h_conv2_dim])
         
         # fc layers
-        '''
-        W_fc1 = self.weight_variable([h_conv2_dim,128])
-        b_fc1 = self.bias_variable([128])
-        
-        W_fc2 = self.weight_variable([128,1])
-        b_fc2 = self.bias_variable([1])
-        
-        h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat,W_fc1) + b_fc1)
-        
-        self.up_probability = tf.sigmoid(tf.matmul(h_fc1,W_fc2) + b_fc2)
-        '''
         h = tf.layers.dense(h_conv2_flat,units=128,activation=tf.nn.relu,
-            kernel_initializer=tf.contrib.layers.xavier_initializer())
+                            kernel_initializer=tf.contrib.layers.xavier_initializer())
         self.up_probability = tf.layers.dense(h,units=1,activation=tf.sigmoid,
-            kernel_initializer=tf.contrib.layers.xavier_initializer())
-        self.loss = tf.losses.log_loss(labels=self.sampled_actions,predictions=self.up_probability,weights=self.advantage)
+                                              kernel_initializer=tf.contrib.layers.xavier_initializer())
+        self.loss = tf.losses.log_loss(labels=self.sampled_actions,
+                                       predictions=self.up_probability,
+                                       weights=self.advantage)
         #optimizer = tf.train.AdamOptimizer(self.learning_rate)
         optimizer = tf.train.RMSPropOptimizer(self.learning_rate)
         self.train_op = optimizer.minimize(self.loss)
         
     def train_network(self, states, actions, rewards):
-        print("Training with %d tuples" % len(states))
+        print('Training with %d tuples' % len(states))
         
         states = np.array(states)
         actions = np.vstack(actions)
@@ -242,18 +205,17 @@ class Agent_PG(Agent):
             discounted_rewards[t] = discounted_reward_sum
         return discounted_rewards
     
-    
     def forward_pass(self, observations):
         up_probability = self.sess.run(self.up_probability,
             feed_dict={self.observations: observations[np.newaxis,:]})
         return up_probability
 
     def load_checkpoint(self):
-        print("Loading checkpoint...")
+        print('Loading checkpoint...')
         self.saver.restore(self.sess, self.checkpoint_file)
 
     def save_checkpoint(self):
-        print("Saving checkpoint...")
+        print('Saving checkpoint...')
         self.saver.save(self.sess, self.checkpoint_file)
 
     def weight_variable(self,shape):
@@ -265,7 +227,7 @@ class Agent_PG(Agent):
         return tf.Variable(initial)
         
     def conv2d(self,x, W, stride):
-        return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "VALID")
+        return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = 'VALID')
     
     def prepro(self, o, image_size=[80,80]):
         """
