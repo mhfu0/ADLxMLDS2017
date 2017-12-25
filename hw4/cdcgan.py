73fa81e2744d14702f4cdbee085140f1c5e15462
mhfu0 <r06922030@ntu.edu.tw>
2017-12-25 14:41:41 +0800

    small fix

--- hw4/cdcgan.py
+++ hw4/cdcgan.py
@@ -18,10 +18,10 @@ import scipy.misc
 # Create feature indices mapping
 hair_colors = ['orange hair', 'white hair', 'aqua hair', 'gray hair', 
                'green hair', 'red hair', 'purple hair', 'pink hair',
-               'blue hair', 'black hair', 'brown hair', 'blonde hair']
+               'blue hair', 'black hair', 'brown hair', 'blonde hair', '<unk_hair>']
 eye_colors = ['gray eyes', 'black eyes', 'orange eyes', 'pink eyes',
               'yellow eyes', 'aqua eyes', 'purple eyes', 'green eyes', 
-              'brown eyes', 'red eyes', 'blue eyes', 'bicolored eyes']
+              'brown eyes', 'red eyes', 'blue eyes', '<unk_eyes>']
 
 index2hair = dict(list(enumerate(hair_colors)))
 index2eye = dict(list(enumerate(eye_colors)))
@@ -29,94 +29,97 @@ index2eye = dict(list(enumerate(eye_colors)))
 hair2index = dict(list(zip(index2hair.values(), index2hair.keys())))
 eye2index = dict(list(zip(index2eye.values(), index2eye.keys())))
 
-########## Load tag data ########## 
-print('Load tag data')
+y_dim = len(hair_colors) + len(eye_colors)  # y_dim = 25
+z_dim = y_dim*4  # z_dim = 100
 
-file = open('tags_clean.csv', 'r')
-text_labels = []
-
-MANUAL_TAGGING = False
-
-for line in file:
-    line = line.rstrip('\n\t').split(',')
-    img_id = line[0]
-
-    line = line[1]
-    raw_tags = line.split('\t')
-    raw_tags = list(map(lambda x: x.split(':')[0], raw_tags)) 
-
-    pair = [None, None] 
-    for t in raw_tags:
-        if t in hair_colors:
-            pair[0] = t
-        if t in eye_colors: 
-            pair[1] = t
-
-    '''
-    MANUAL_TAGGING = True
-    if not pair[0] or not pair[1]:
-        tmp_img = skimage.io.imread('faces/%s.jpg' % img_id)
+########## Load data ########## 
+try:
+    with open('img_data_new.pkl', 'rb') as f:
+        img_data = pickle.load(f)
+    with open('label_data_new.pkl', 'rb') as f:
+        label_data = pickle.load(f)
+    print('Preprocessed data loaded')
+    
+except:
+    MANUAL_TAGGING = False
+    
+    print('Load tag data')
+    file = open('tags_clean.csv', 'r')
+    text_labels = []
+    
+    for line in file:
+        line = line.rstrip('\n\t').split(',')
+        img_id = line[0]
+    
+        line = line[1]
+        raw_tags = line.split('\t')
+        raw_tags = list(map(lambda x: x.split(':')[0], raw_tags)) 
+    
+        pair = ([], []) 
+        for t in raw_tags:
+            if t in hair_colors: pair[0].append(t)
+            if t in eye_colors:  pair[1].append(t)
+    
+        '''
+        MANUAL_TAGGING = True
+        if not pair[0] or not pair[1]:
+            tmp_img = skimage.io.imread('faces/%s.jpg' % img_id)
+            
+            plt.imshow(tmp_img)
+            plt.show()
+            
+            if not pair[0]:
+                pair[0] = str(input()) + ' hair'
+            if not pair[1]:
+                pair[1] = str(input()) + ' eyes'
         
-        plt.imshow(tmp_img)
-        plt.show()
+        print('faces/%s.jpg' % img_id)
+        print(pair)
         
-        if not pair[0]:
-            pair[0] = str(input()) + ' hair'
-        if not pair[1]:
-            pair[1] = str(input()) + ' eyes'
+        if int(img_id) > 0 and int(img_id) % 10 == 0:
+            clear_output()
+        '''
+        text_labels.append(pair)
+        
+    file.close()
     
-    print('faces/%s.jpg' % img_id)
-    print(pair)
+    ##########  End loading tag data ##########
+    print('End loading %d tag data' % len(text_labels))
     
-    if int(img_id) > 0 and int(img_id) % 10 == 0:
-        clear_output()
     '''
-    text_labels.append(pair)
-    
-file.close()
-with open('text_labels.pkl', 'wb') as p:
-    pickle.dump(text_labels, p)
-
-##########  End loading tag data ##########
-print('End loading %d tag data' % len(text_labels))
-
-idx_label_tuples = []
-if not MANUAL_TAGGING:
+    idx_label_tuples = []
     for idx, label in enumerate(text_labels):
         if label[0] in hair_colors and label[1] in eye_colors:
             idx_label_tuples.append((idx, label))
         else:
             pass
-
-print('Pick %d data' % len(idx_label_tuples))
-
-########## Load image data ########## 
-print('Load image data')
-try:
-    with open('img_data.pkl', 'rb') as f:
-        img_data = pickle.load(f)
-    with open('label_data.pkl', 'rb') as f:
-        label_data = pickle.load(f)
-        
-except:
+    
+    print('Pick %d data' % len(idx_label_tuples))
+    '''
+    
+    ########## Load image data ##########         
     print('Load raw image data')
-    idx_list = [tup[0] for tup in idx_label_tuples]
-
     img_list = []
     label_list = []
 
-    for tup in idx_label_tuples:
+    for idx, pair in enumerate(text_labels):
         # Read image
-        idx = tup[0]
         img = skimage.io.imread('faces/%d.jpg' % idx)
         img_r = skimage.transform.resize(img, (64,64))
-
+        
         # encode labels
-        hair_idx = hair2index[tup[1][0]]
+        hair_idx_list = pair[0]
+        eye_idx_list = pair[1]
+        
+        # skip if both labels not valid
+        if (not hair_idx_list) and (not eye_idx_list):
+            continue
+        
+        hair_idx = hair2index[hair_idx_list[0]] if hair_idx_list else hair2index['<unk_hair>']
         hair_one_hot = np.zeros(len(hair2index))
         hair_one_hot[hair_idx] = 1.
-
-        eye_idx = eye2index[tup[1][1]]
+        
+        eye_idx = eye2index[eye_idx_list[0]] if eye_idx_list else eye2index['<unk_eyes>']
         eye_one_hot = np.zeros(len(eye2index))
         eye_one_hot[eye_idx] = 1.
 
@@ -127,22 +130,23 @@ except:
     img_data = np.array(img_list)
     label_data = np.array(label_list)
 
-    with open('img_data.pkl', 'wb') as f:
+    with open('img_data_new.pkl', 'wb') as f:
         pickle.dump(img_data, f)
-    with open('label_data.pkl', 'wb') as f:
+    with open('label_data_new.pkl', 'wb') as f:
         pickle.dump(label_data, f)
 
 print(img_data.shape, label_data.shape)
 print('End loading %d image data' % len(img_data))
 
-########## End loading image data ##########
+print(label_data[0])
+########## End loading data ##########
 
 def sample_batch(img_data, label_data, batch_size=64):
     assert(len(img_data)==len(label_data))
     
     order = np.random.choice(np.arange(len(img_data)), batch_size)
     
-    # data augmentation
+    # TODO: data augmentation
     
     
     return img_data[order], label_data[order]
@@ -213,23 +217,24 @@ def Discriminator(x, y, reuse=False):
         conv3 = tf.layers.batch_normalization(conv3, training=True)
         conv3 = leaky_relu(conv3)
         
-        fc = tc.layers.flatten(conv3)
-        fc = tc.layers.fully_connected(fc, 96, weights_initializer=tf.random_normal_initializer(stddev=0.02), activation_fn=None)
+        flat = tc.layers.flatten(conv3)
+        print(flat.get_shape())
+        fc_y = tf.concat([flat, y], axis=1)
+        fc = tc.layers.fully_connected(fc_y, z_dim+y_dim, weights_initializer=tf.random_normal_initializer(stddev=0.02), activation_fn=None)
         fc = leaky_relu(fc)
         fc = tf.nn.dropout(fc, 0.3)
         
-        fc_y = tf.concat([fc, y], axis=1)
-        print('fc_y.shape =',fc_y.shape)
+        #fc_y = tf.concat([fc, y], axis=1)
         
         fc2 = tc.layers.fully_connected(fc, 1, weights_initializer=tf.random_normal_initializer(stddev=0.02), activation_fn=None)
         
         return tf.nn.sigmoid(fc2)
 
 x = tf.placeholder(tf.float32, shape=[None, 64, 64, 3])
-y = tf.placeholder(tf.float32, shape=[None, 24])
-z = tf.placeholder(tf.float32, shape=[None, 96])
+y = tf.placeholder(tf.float32, shape=[None, y_dim])
+z = tf.placeholder(tf.float32, shape=[None, z_dim])
 
-w = tf.placeholder(tf.float32, shape=[None, 24]) # wrong label
+w = tf.placeholder(tf.float32, shape=[None, y_dim]) # wrong label
 
 G = Generator(z, y, reuse=False)
 D_real = Discriminator(x, y, reuse=False)
@@ -243,12 +248,13 @@ D_fake_wrong = Discriminator(G, w, reuse=True)
 # d_loss for DCGAN
 #d_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real, labels=tf.ones_like(D_real))) + #tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_fake)))
 
-d_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real, labels=tf.ones_like(D_real))) + (tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_fake))) + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_real_wrong))) + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_fake_wrong))))/3
+#d_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real, labels=tf.ones_like(D_real))) + (tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_fake))) + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_real_wrong))) + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_fake_wrong))))/3
+d_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real, labels=tf.ones_like(D_real))) + (tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_fake))) + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_real_wrong))))/2
 
 g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.ones_like(D_fake)))
 
-LR_D = 2e-4
-LR_G = 2e-4
+LR_D = 1e-4
+LR_G = 1e-4
 
 BATCH_SIZE = 64
 
@@ -258,7 +264,7 @@ g_vars = [var for var in tf.global_variables() if "g_net" in var.name]
 d_updates = tf.train.AdamOptimizer(LR_D, beta1=0.5, beta2=0.9).minimize(d_loss, var_list=d_vars)
 g_updates = tf.train.AdamOptimizer(LR_G, beta1=0.5, beta2=0.9).minimize(g_loss, var_list=g_vars)
 
-TRAIN = False
+TRAIN = True
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -272,15 +278,14 @@ with tf.Session(config=config) as sess:
     if not TRAIN:
         print('Loading model parameters...')
         saver.restore(sess, checkpoint_path)
-        
-        test_rand = np.random.uniform(0., 1., size=[5, 96])
-        test_label = np.zeros(24)
+        test_rand = np.random.uniform(0., 1., size=[5, z_dim])
+        test_label = np.zeros(y_dim)
         test_label[np.array([5,19])] = 1.
         test_label = np.tile(test_label, (5,1))
         
         data = sess.run(G, {z: test_rand, y: test_label})
-        for i in range(5)
-            scipy.misc.imsave('result/result_%d.jpg' % i, np.squeeze(data[i]))
+        for i in range(5):
+            scipy.misc.imsave('early/sample_1_%d.jpg' % (i+1), np.squeeze(data[i]))
         
     else:
         d_loss_vals = []
@@ -289,14 +294,17 @@ with tf.Session(config=config) as sess:
         for ep in range(200):
             for i in range(500):
                 imgs, labels = sample_batch(img_data, label_data)
+                
                 w_labels = np.zeros_like(labels)
                 w_labels[np.random.choice(np.where(labels==0.)[0],2)]=1.
 
-                rand = np.random.uniform(0., 1., size=[BATCH_SIZE, 96])
+                rand = np.random.uniform(0., 1., size=[BATCH_SIZE, z_dim])
                 _, d_loss_curr = sess.run([d_updates, d_loss], {x: imgs, z: rand, y: labels, w: w_labels})
-
-                rand = np.random.uniform(0., 1., size=[BATCH_SIZE, 96])
-                _, g_loss_curr = sess.run([g_updates, g_loss], {z: rand, y: labels})
+                
+                gen_train_step = 1
+                if i % gen_train_step == 0:
+                    rand = np.random.uniform(0., 1., size=[BATCH_SIZE, z_dim])
+                    _, g_loss_curr = sess.run([g_updates, g_loss], {z: rand, y: labels})
 
                 d_loss_vals.append(d_loss_curr)
                 g_loss_vals.append(g_loss_curr)            
@@ -304,13 +312,13 @@ with tf.Session(config=config) as sess:
                 print("%d / %d: %e, %e" % (i, 500, d_loss_curr, g_loss_curr))
             
             # output result
-            test_rand = np.random.uniform(0., 1., size=[1, 96])
-            test_label = np.zeros(24)
+            test_rand = np.random.uniform(0., 1., size=[1, z_dim])
+            test_label = np.zeros(y_dim)
             test_label[np.array([5,19])] = 1.
             test_label = test_label[np.newaxis,:]
             
             data = sess.run(G, {z: test_rand, y: test_label})
-            scipy.misc.imsave('result/outfile_%d.jpg' % ep, np.squeeze(data[0]))
+            scipy.misc.imsave('result/train_output_%d.jpg' % ep, np.squeeze(data[0]))
             print('save image on epoch %d' % ep)
             
             print('Saving model parameters...')
